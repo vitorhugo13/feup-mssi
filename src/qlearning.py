195f33f4d6752cef47ebfb1a0ff79a0edc3e32ba
vitorhugo13 <vhlg99@gmail.com>
2021-06-01 10:41:01 +0100

    choose_action and update_table skeleton

--- src/qlearning.py
+++ src/qlearning.py
@@ -12,23 +12,30 @@ STATE_SIZE = 1
 ACTION_SIZE = 2
 
 # initialize our Q-table
-def create_table(STATE_SPACE_SIZE, ACTION_SPACE_SIZE):
-    q_table = np.zeros((STATE_SPACE_SIZE, ACTION_SPACE_SIZE))
-    return q_table
+QLEARNING_TABLE = np.zeros((STATE_SIZE, ACTION_SIZE))
 
 # choose action that agent will choose
-def choose_action(table):
-    print (table)
-    return
+def choose_action():
+    max_utility = np.argmax(QLEARNING_TABLE, axis=1)
+    return max_utility
 
 # update table using some qlearning equation
-def update_table():
+def update_table(index, value):
+    QLEARNING_TABLE[0, index] = value
     return
 
 # entry point
 def main():
-    qlearning_table = create_table(STATE_SIZE, ACTION_SIZE)
-    choose_action(qlearning_table)
+    print(QLEARNING_TABLE)
+    
+    print(choose_action())
+    update_table(1,50)
+    print(choose_action())
+    update_table(0,90)
+    print(choose_action())
+
+    print(QLEARNING_TABLE)
+
     return
 
 if __name__ == '__main__':
